da11ef1b502fa3981c59a9c7789a4b093410518d
Valery <alfonzzagrabaster@gmail.com>
2022-06-30 17:58:25 +0300

    created .css .html .js files for first pet project

---
+++ projectUno.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const obj = {
+   5:true,
+   '1':false
+}; 
+
+console.log(obj[1]);
+
+const arr = [];
+
+arr[0] = prompt('How do you do, felow kids?', '');
+arr[1] = prompt('Answer','');
+arr[2] = prompt('2+3?', '');
+
+
+document.write(arr);
